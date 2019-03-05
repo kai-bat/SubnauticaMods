c869417a60a5f17656abe4135d59d5878b3c6196
kylinator25 <kmcg02@outlook.com>
2019-03-05 19:29:40 +1100

    added unstaged files from prev commit
    Signed-off-by: kylinator25 <kmcg02@outlook.com>

---
+++ Shark/DestroyPatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using Object = UnityEngine.Object;
+using UnityEngine;
+
+namespace Shark
+{
+    [HarmonyPatch(typeof(Object))]
+    [HarmonyPatch("Destroy")]
+    [HarmonyPatch(new Type[]
+    {
+        typeof(Object)
+    })]
+    public class DestroyPatch
+    {
+        public static bool Prefix(Object __instance, ref Object obj)
+        {
+            try
+            {
+                Console.WriteLine($"{obj.name} destroyed at {Environment.StackTrace}");
+                if(((GameObject)obj).GetComponentInChildren<Shark>()) {
+                    Console.WriteLine("THE ABOVE OBJECT CONTAINED THE SHARK, CANCELLING DESTRUCTION");
+                    return false;
+                }
+            }
+            catch
+            {
+            }
+            return true;
+        }
+    }
+}
---
+++ Shark/SharkControl.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using SMLHelper.V2.MonoBehaviours;
+using Object = UnityEngine.Object;
+
+namespace Shark
+{
+    public class SharkControl : MonoBehaviour
+    {
+        public Shark shark;
+
+        void Awake()
+        {
+            shark.useRigidbody = GetComponent<Rigidbody>();
+            shark.useRigidbody.angularDrag = 1f;
+        }
+
+        void OnDestroy()
+        {
+            if(shark.GetPilotingMode())
+            {
+                if(!Player.main.ToNormalMode(true))
+                {
+                    Player.main.ToNormalMode(false);
+                }
+
+                Player.main.transform.parent = null;
+            }
+        }
+
+        void Update()
+        {
+            Fixer fixer = shark.GetComponent<Fixer>();
+            if (fixer)
+            {
+                Object.Destroy(fixer);
+            }
+
+            if (shark.GetPilotingMode() && !shark.worldForces.IsAboveWater())
+            {
+                shark.isBoosting = GameInput.GetButtonHeld(GameInput.Button.Sprint);
+
+                if(shark.isBoosting)
+                {
+                    shark.useRigidbody.AddForce(transform.forward * 10f, ForceMode.VelocityChange);
+                }
+                else
+                {
+                    shark.useRigidbody.AddForce(transform.rotation * GameInput.GetMoveDirection() * 5f);
+                }
+
+                Vector2 look = GameInput.GetLookDelta();
+
+                shark.useRigidbody.AddTorque(transform.up * look.x * 0.01f, ForceMode.VelocityChange);
+                shark.useRigidbody.AddTorque(transform.right * -look.y * 0.01f, ForceMode.VelocityChange);
+                shark.useRigidbody.AddTorque(transform.forward * -look.x * 0.01f, ForceMode.VelocityChange);
+
+                shark.StabilizeRoll();
+            }
+        }
+    }
+}
